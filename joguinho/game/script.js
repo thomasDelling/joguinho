14a1ef37341a0417c45f510c48185aae87aa4f7d
Thomas Delling <162994284+thomasDelling@users.noreply.github.com>
2025-03-02 20:04:02 -0300

    Add files via upload

---
+++ joguinho/game/script.js
@@ -0,0 +1,5 @@
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "Escape") { // Verifica se a tecla pressionada é o ESC
+            window.location.href = "../index.html"; // Redireciona para o menu do jogo
+        }
+    });
